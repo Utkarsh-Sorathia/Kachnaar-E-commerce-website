d975de70b75f7029c07a9ab98e5b3316e3d059bf
Utkarsh-2003 <utkarshsor03@gmail.com>
2024-01-26 19:30:52 +0530

    layout

--- src/Routers/Layout.js
+++ src/Routers/Layout.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Layout = () => {
   return (
     <div>
-      <button type="button" class="btn btn-outline-success"><Link to='/'>Sign-in</Link></button>
+      <Link to='/' class="btn btn-outline-success">Sign-in</Link>
       <Link to='/register'>Register</Link>
     </div>
   )
