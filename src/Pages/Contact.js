29992a5ef4f723f661ee61c8a6eb7c1df3c096bf
Utkarsh-2003 <utkarshsor03@gmail.com>
2024-03-01 17:16:31 +0530

    email js key

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,73 +1,36 @@
 import React, { useRef } from "react";
-import Navbar from "./Navbar";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-    // service_id, templte_id and public key will get from Emailjs website when you create account and add template service and email service
+
     emailjs
-      .sendForm(
-        process.env.REACT_APP_SERVICE,
-        process.env.REACT_APP_TEMPLATE,
-        form.current,
-        process.env.REACT_APP_PUBLIC
-      )
+      .sendForm(process.env.REACT_APP_SERVICE, process.env.REACT_APP_TEMPLATE, form.current, {
+        publicKey: process.env.REACT_APP_PUBLIC,
+      })
       .then(
-        (result) => {
-          console.log(result.text);
+        () => {
+          console.log("SUCCESS!");
         },
         (error) => {
-          console.log(error.text);
+          console.log("FAILED...", error.text);
         }
       );
   };
 
   return (
-    <>
-      <Navbar />
-      <div
-        className="container border mt-3"
-        style={{ height: "500px", width: "500px", borderRadius: "10px" }}
-      >
-        <form
-          className="form-control border-0"
-          ref={form}
-          onSubmit={(e) => sendEmail(e)}
-        >
-          <h1 className="text-center mt-2">Contact Form</h1>
-          <div className="form-outline p-3">
-            <input
-              type="email"
-              className="form-control form-control-lg"
-              placeholder="Email address"
-              required
-            />
-          </div>
-          <div className="form-outline p-3">
-            <input
-              type="text"
-              className="form-control form-control-lg"
-              placeholder="Name"
-              required
-            />
-          </div>
-          <div className="form-outline p-3">
-            <textarea
-              placeholder="Message..."
-              className="form-control form-control-lg"
-            />
-          </div>
-          <div className="form-outline p-3 text-center">
-            <button type="submit" className="btn btn-primary">
-              Send Message
-            </button>
-          </div>
-        </form>
-      </div>
-    </>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
   );
 };
 
